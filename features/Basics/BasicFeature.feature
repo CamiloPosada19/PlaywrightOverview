5ba0d066ce3942a542887b0da07abdea2f919567
Camilo Posada <90696295+CamiloPosada19@users.noreply.github.com>
2024-03-03 12:00:53 +0100

    Feat: Use of basic locators and some validations

--- features/Basics/BasicFeature.feature
+++ features/Basics/BasicFeature.feature
@@ -6,9 +6,12 @@ I want to login into applications
 
 Scenario: Basic actions with different selectors 
 	Given I as a user enter the website
-	#When I click and type on an element with ID
-	#And I click and type on an element with Class
-	#And I click and type on an element with XPath
-	#Then I verify that the actions are performed correctly
+	When I verify and type on an element with ID
+	And I click and type on an element with Class
+	And I click and type on an element with XPath
+	Then I verify that the actions are performed correctly
 
-	
+# Scenario: Specialized playwright locators
+# 	Given I as user click and upload document with locator getByrole
+# 	When  Search for an element by text aand another by placeholder
+# 	Then I select the checks by the label 
--- stepDefinitions/BasicFeatureSteps.js
+++ stepDefinitions/BasicFeatureSteps.js
@@ -1,39 +1,42 @@
 const {Given,When,Then} = require('@cucumber/cucumber');
-const { faker } = require('@faker-js/faker');
+const { faker, NameModule } = require('@faker-js/faker');
+//const { expect } = require('chai');
+const { expect } = require('@playwright/test');
+// import { test, expect } from '';
 
-require('dotenv').config({path:"env/.env"})
+objectData = {
+  randomName:faker.person.firstName(),
+  randomEmail:faker.internet.email(),
+  randomText:faker.hacker.phrase()
+}
 
 Given("I as a user enter the website",async function(){
-  await page.goto(process.env.URLFORM)
-  await page.locator(".fc-button-label").first().click();
-  const randomFullname = faker.person.fullName();   // test faker.js
-  console.log(randomFullname)
-  
- 
-  
-  
+  await page.goto(process.env.URLBASE)
 })
 
 
-When('I click and type on an element with ID',async function () {
-await page.locator("#firstName").click()
+When('I verify and type on an element with ID',async function () {
+  const nameID = await page.locator("#carouselExampleIndicators")
+  await expect(nameID,"The element is not found").toBeVisible()
+
 });
 
 
-When('I click and type on an element with Class', function () {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("Conectado")
+When('I click and type on an element with Class', async function () {
+    await page.locator(".nav-link").filter({hasText:'Contact'}).click();
 });
 
 
-When ('I click and type on an element with XPath', function () {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("Conectado")
+When ('I click and type on an element with XPath', async function () {
+  await page.locator("//input[@id='recipient-email']").fill(objectData.randomEmail);
+  await page.locator("//input[@id='recipient-name']").fill(objectData.randomName);
+  await page.locator("//textarea[contains(@class,'form-control')]").fill(objectData.randomText);
+  await page.locator("//button[@type='button'][contains(.,'Send message')]").click();
 });
 
 
 
-Then('I verify that the actions are performed correctly', function () {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("Conectado")
+Then('I verify that the actions are performed correctly', async function () {
+  await expect(page.locator('img[alt="First slide"]')).toBeVisible();
+  await expect(page.locator("#nava")).toHaveText("PRODUCT STORE")
 });
