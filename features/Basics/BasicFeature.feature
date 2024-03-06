317ef3154a608fcc290d9c3583d46881b17f48ef
Camilo Posada <90696295+CamiloPosada19@users.noreply.github.com>
2024-03-06 07:17:41 +0100

    Feat: Use ob basic locators of playwright

--- features/Basics/BasicFeature.feature
+++ features/Basics/BasicFeature.feature
@@ -1,17 +1,21 @@
 @basics
-Feature: Basic actions 
+Feature: Basic actions
 
-As a user
-I want to login into applications
+	I as a user want to use the different selectors of playwright
 
-Scenario: Basic actions with different selectors 
-	Given I as a user enter the website
-	When I verify and type on an element with ID
-	And I click and type on an element with Class
-	And I click and type on an element with XPath
-	Then I verify that the actions are performed correctly
+	Background:
+		Given I as a user enter the website "ecommerce"
 
-# Scenario: Specialized playwright locators
-# 	Given I as user click and upload document with locator getByrole
-# 	When  Search for an element by text aand another by placeholder
-# 	Then I select the checks by the label 
+	Scenario: Basic actions with different selectors
+		When I verify and type on an element with ID
+		And I click and type on an element with Class
+		And I click and type on an element with XPath
+		Then I verify that the actions are performed correctly
+
+	Scenario: Specialized playwright locators
+		Given I as user click in laptop with getByRole
+		When  Click on an element by text
+		Then I check the element with text
+
+
+	
--- stepDefinitions/BasicFeatureSteps.js
+++ stepDefinitions/BasicFeatureSteps.js
@@ -1,42 +1,65 @@
-const {Given,When,Then} = require('@cucumber/cucumber');
+const { Given, When, Then } = require('@cucumber/cucumber');
 const { faker, NameModule } = require('@faker-js/faker');
 //const { expect } = require('chai');
 const { expect } = require('@playwright/test');
 // import { test, expect } from '';
 
 objectData = {
-  randomName:faker.person.firstName(),
-  randomEmail:faker.internet.email(),
-  randomText:faker.hacker.phrase()
+  randomName: faker.person.firstName(),
+  randomEmail: faker.internet.email(),
+  randomText: faker.hacker.phrase()
 }
 
-Given("I as a user enter the website",async function(){
-  await page.goto(process.env.URLBASE)
+Given("I as a user enter the website {string}", async function (environment) {
+
+  switch (environment) {
+    case ("ecommerce"):
+      await page.goto(process.env.URLBASE)
+      break;
+    case ("components"):
+      await page.goto(process.env.URLCOMPONENTS)
+      break;
+    default:
+      console.log("page not recognized")
+
+  }
+
+
+
 })
 
-
-When('I verify and type on an element with ID',async function () {
+When('I verify and type on an element with ID', async function () {
   const nameID = await page.locator("#carouselExampleIndicators")
-  await expect(nameID,"The element is not found").toBeVisible()
-
+  await expect(nameID, "The element is not found").toBeVisible()
 });
-
 
 When('I click and type on an element with Class', async function () {
-    await page.locator(".nav-link").filter({hasText:'Contact'}).click();
+  await page.locator(".nav-link").filter({ hasText: 'Contact' }).click();
 });
 
-
-When ('I click and type on an element with XPath', async function () {
+When('I click and type on an element with XPath', async function () {
   await page.locator("//input[@id='recipient-email']").fill(objectData.randomEmail);
   await page.locator("//input[@id='recipient-name']").fill(objectData.randomName);
   await page.locator("//textarea[contains(@class,'form-control')]").fill(objectData.randomText);
   await page.locator("//button[@type='button'][contains(.,'Send message')]").click();
 });
 
-
-
 Then('I verify that the actions are performed correctly', async function () {
   await expect(page.locator('img[alt="First slide"]')).toBeVisible();
-  await expect(page.locator("#nava")).toHaveText("PRODUCT STORE")
+  await expect(page.locator("#nava")).toHaveText("PRODUCT STORE");
+});
+
+
+Given('I as user click in laptop with getByRole', async function () {
+  await page.getByRole("link", { name: 'Laptops' }).click();
+});
+
+When('Click on an element by text', async function () {
+  await page.getByText("MacBook air").click();
+
+});
+
+Then('I check the element with text', async function () {
+  await expect(page.getByText('Product description')).toBeVisible();
+  // await expect(title,"the title has not been found").toHaveText("MacBook Air")
 });
